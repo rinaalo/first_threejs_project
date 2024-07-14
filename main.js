24515fddc0351cea30cfb519168067412ebbbc75
alor <93737038+rinaalo@users.noreply.github.com>
2024-07-15 01:35:43 +0200

    - added  pillars - positioned pillars and bear

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
-const d = 8;
+const d = 10;
 const aspectRatio = innerWidth / innerHeight;
 //const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const camera = new THREE.OrthographicCamera(-d * aspectRatio, d * aspectRatio, d, -d, 1, 1000);
@@ -21,7 +21,7 @@ const plane_color = 0x7a4aff;
 const planeGeometry = new THREE.PlaneGeometry(plane_width, plane_height);
 const planeMaterial = new THREE.MeshPhongMaterial({ color: plane_color, side: THREE.DoubleSide });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.position.set(0, -2.2, 0);
+plane.position.set(0, 0, 0);
 plane.rotation.x = Math.PI / 2
 scene.add(plane);
 
@@ -41,15 +41,62 @@ function animate() {
 }
 renderer.setAnimationLoop(animate);
 
+function get_size(gltf_object) {
+    const box = new THREE.Box3().setFromObject( gltf_object ); 
+    return box.getSize(new THREE.Vector3());
+}
+
 //-- Load bear model --//
 const loader = new GLTFLoader();
 let bear;
 loader.load('assets/character1.glb', function (gltf) {
     bear = gltf.scene;
+    const bear_size = get_size(bear);
+    bear.position.set(5, bear_size.y/2 + 0.5, 0);
     scene.add(bear);
 }, undefined, function (error) {
     console.error(error);
 });
 
-//-- bear movement --//
-const bear_speed = 1;
+
+//-- Load pillars --//
+let pillar1;
+loader.load('assets/pillar.glb', function (gltf) {
+    pillar1 = gltf.scene;
+    const pillar1_size = get_size(pillar1);
+    pillar1.position.set(-plane_width/2 + pillar1_size.z/2, pillar1_size.y/2, -plane_height/2 + pillar1_size.z/2);
+    scene.add(pillar1);
+}, undefined, function (error) {
+    console.error(error);
+});
+
+let pillar2;
+loader.load('assets/pillar.glb', function (gltf) {
+    pillar2 = gltf.scene;
+    const pillar2_size = get_size(pillar2);
+    pillar2.position.set(-plane_width/2 + pillar2_size.z/2, pillar2_size.y/2, plane_height/2 - pillar2_size.z/2);
+    scene.add(pillar2);
+}, undefined, function (error) {
+    console.error(error);
+});
+
+let pillar3;
+loader.load('assets/pillar.glb', function (gltf) {
+    pillar3 = gltf.scene;
+    const pillar3_size = get_size(pillar3);
+    pillar3.position.set(plane_width/2 - pillar3_size.z/2, pillar3_size.y/2, plane_height/2 - pillar3_size.z/2);
+    scene.add(pillar3);
+}, undefined, function (error) {
+    console.error(error);
+});
+
+let pillar4;
+loader.load('assets/pillar.glb', function (gltf) {
+    pillar4 = gltf.scene;
+    const pillar4_size = get_size(pillar4);
+    pillar4.position.set(plane_width/2 - pillar4_size.z/2, pillar4_size.y/2, - plane_height/2 + pillar4_size.z/2);
+    scene.add(pillar4);
+}, undefined, function (error) {
+    console.error(error);
+});
+
