3be6b415d0cd44da0b5e795d2f0a3997822ffd4d
alor <93737038+rinaalo@users.noreply.github.com>
2024-07-15 00:44:36 +0200

    - fixed plane rotation - changed color of light, plane - removed counter.js and style.css

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { rotate } from 'three/examples/jsm/nodes/Nodes.js';
-
 
 const scene = new THREE.Scene();
-const d = 5;
+const d = 8;
 const aspectRatio = innerWidth / innerHeight;
 //const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const camera = new THREE.OrthographicCamera(-d * aspectRatio, d * aspectRatio, d, -d, 1, 1000);
@@ -16,31 +14,34 @@ camera.lookAt(new THREE.Vector3(0, 0, 0))
 renderer.setSize(innerWidth, innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const plane_width = 5;
-const plane_height = 5;
+//-- Plane stuff --//
+const plane_width = 20;
+const plane_height = 20;
+const plane_color = 0x7a4aff;
 const planeGeometry = new THREE.PlaneGeometry(plane_width, plane_height);
-const planeMaterial = new THREE.MeshPhongMaterial({ color: 0x0000ff, side: THREE.DoubleSide });
+const planeMaterial = new THREE.MeshPhongMaterial({ color: plane_color, side: THREE.DoubleSide });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.position.set(0, -2, 0);
-plane.setRotationFromEuler(new THREE.Euler(90, 0, 0));
+plane.position.set(0, -2.2, 0);
+plane.rotation.x = Math.PI / 2
 scene.add(plane);
 
+const pillar_padding = 5;
 
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(0, 0, 1);
+//-- Light stuff --//
+const light = new THREE.DirectionalLight(0xffc0b0, 2);
+light.position.set(0, 1, 0);
 scene.add(light);
-const ambientLight = new THREE.AmbientLight(0x404040);
+const ambientLight = new THREE.AmbientLight(0x404040, 2);
 scene.add(ambientLight);
 
+//-- Animate --//
 function animate() {
-    //plane.rotation.x += 0.01;
-    camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.01);
+    //camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.01);
     renderer.render(scene, camera);
-    console.log(bear)
 }
 renderer.setAnimationLoop(animate);
 
-
+//-- Load bear model --//
 const loader = new GLTFLoader();
 let bear;
 loader.load('assets/character1.glb', function (gltf) {
@@ -49,3 +50,6 @@ loader.load('assets/character1.glb', function (gltf) {
 }, undefined, function (error) {
     console.error(error);
 });
+
+//-- bear movement --//
+const bear_speed = 1;
